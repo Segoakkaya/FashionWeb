e7fcf98a4e52c10a50d5b97d28c2de4c87c7cefa
Segoakkaya <akkayasergen59@gmail.com>
2023-10-02 17:38:10 +0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+//require("../node_modules/jquery/dist/jquery")
+window.$ = $;
+window.jQuery = $;
+jQuery = $;
+    require("../node_modules/bootstrap/dist/js/bootstrap"),
+    require("../node_modules/owl.carousel/dist/owl.carousel.min"),
+    require("./js/script");
